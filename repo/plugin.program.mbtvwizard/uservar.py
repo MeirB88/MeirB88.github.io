7d2e05b85e3c2af7bdb5508fb4483fa83417b372
mb_10 <meir1998@gmail.com>
2024-06-05 18:35:52 +0300

    no message

--- repo/plugin.program.mbtvwizard/uservar.py
+++ repo/plugin.program.mbtvwizard/uservar.py
@@ -60,8 +60,8 @@ SPACER = '='
 
 # You can edit these however you want, just make sure that you have a %s in each of the
 # THEME's so it grabs the text from the menu item
-COLOR1 = '00132b'
-COLOR2 = '002f4e'
+COLOR1 = 'mediumblue'
+COLOR2 = 'royalblue'
 # Primary menu items   / {0} is the menu item and is required
 THEME1 = u'[COLOR {color1}][I]([COLOR {color1}][B]Open[/B][/COLOR][COLOR {color2}]Wizard[COLOR {color1}])[/I][/COLOR] [COLOR {color2}]{{}}[/COLOR]'.format(color1=COLOR1, color2=COLOR2)
 # Build Names          / {0} is the menu item and is required
